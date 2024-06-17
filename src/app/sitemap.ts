c2df7e2c63820c0704a2034129bb5f399138c683
rEjiN2 <rejin374@gmail.com>
2024-06-17 17:56:31 +0530

    sitemap added

---
+++ src/app/sitemap.ts
@@ -0,0 +1,36 @@
+import { MetadataRoute } from "next";
+
+export default async function sitemap():Promise<MetadataRoute.Sitemap> {
+    return [
+        {
+            url:`https://www.savehubonline.com`,
+            lastModified: new Date(),
+            changeFrequency: 'yearly',
+            priority: 1,
+        },
+        {
+      url: 'https://www.savehubonline.com/accessories',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: 'https://www.savehubonline.com/electronics',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.8,
+    },
+    {
+        url: 'https://www.savehubonline.com/clothing/womens',
+        lastModified: new Date(),
+        changeFrequency: 'weekly',
+        priority: 0.8,
+      },
+      {
+        url: 'https://www.savehubonline.com/clothing/mens',
+        lastModified: new Date(),
+        changeFrequency: 'weekly',
+        priority: 0.8,
+      },
+    ]
+}
