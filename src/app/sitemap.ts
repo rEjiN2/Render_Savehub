11fa9f96ef48c2244886a301bc5582e4808f7ace
rEjiN2 <rejin374@gmail.com>
2024-06-17 18:22:29 +0530

    indexing

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,7 +3,7 @@ import { MetadataRoute } from "next";
 export default async function sitemap():Promise<MetadataRoute.Sitemap> {
     return [
         {
-            url:`https://www.savehubonline.com`,
+            url:`https://www.savehubonline.com/`,
             lastModified: new Date(),
             changeFrequency: 'yearly',
             priority: 1,
