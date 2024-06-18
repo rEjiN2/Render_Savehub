2405c7917f53d051f35e14cb7d1df11c80d226ef
rEjiN2 <rejin374@gmail.com>
2024-06-18 12:01:54 +0530

    ads

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,7 +3,7 @@ import { MetadataRoute } from "next";
 export default async function sitemap():Promise<MetadataRoute.Sitemap> {
     return [
         {
-            url:`https://www.savehubonline.com/`,
+            url:'https://www.savehubonline.com/',
             lastModified: new Date(),
             changeFrequency: 'yearly',
             priority: 1,
@@ -18,19 +18,19 @@ export default async function sitemap():Promise<MetadataRoute.Sitemap> {
       url: 'https://www.savehubonline.com/electronics',
       lastModified: new Date(),
       changeFrequency: 'weekly',
-      priority: 0.8,
+      priority: 0.7,
     },
     {
         url: 'https://www.savehubonline.com/clothing/womens',
         lastModified: new Date(),
         changeFrequency: 'weekly',
-        priority: 0.8,
+        priority: 0.9,
       },
       {
         url: 'https://www.savehubonline.com/clothing/mens',
         lastModified: new Date(),
         changeFrequency: 'weekly',
-        priority: 0.8,
+        priority: 0.6,
       },
     ]
 }
