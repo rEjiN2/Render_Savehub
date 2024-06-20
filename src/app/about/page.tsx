7d8cc74035e75b17f03ca417bae77089e9753192
rEjiN2 <rejin374@gmail.com>
2024-06-20 13:09:05 +0530

    about

---
+++ src/app/about/page.tsx
@@ -0,0 +1,115 @@
+import { Box } from '@mui/material'
+import React from 'react'
+
+const AboutUs = () => {
+  return (
+    <div className='text-white flex items-center justify-center pt-5 flex-col'>
+        <div className='text-3xl underline'>
+         AboutUs
+        </div>
+        
+
+          <Box sx={{ display: { xs: 'none', sm: 'none', md: 'none', lg: 'block' } }}>
+              <div className='relative h-[1000px] w-screen container'>
+                  <div className='absolute top-5 left-36 right-10  h-[600px] w-[1000px] p-10 bg-gray-100 rounded-lg z-10'>
+
+                      <h1 className='text-slate-800 text-3xl text-center'>Welcome to <span className='text-purple-700'>SaveHub!</span> </h1>
+
+                      <p className='text-slate-800 mt-10 text-center'>At SaveHub, we're dedicated to bringing you the best loot deals from top online shopping destinations like Amazon, Flipkart, Myntra, and more. Our mission is simple: to help you save money while shopping online by providing the most amazing deals and discounts available across the internet.</p>
+                      <h1 className='text-slate-800 mt-10 text-2xl text-center'>Who We Are?</h1>
+                      <p className='text-slate-800 mt-2 text-center'>SaveHub is a passionate team of deal hunters and shopping enthusiasts who understand the thrill of finding a great bargain. We constantly scour the web to bring you the latest and greatest deals, ensuring that you never miss out on an opportunity to save big. </p>
+                      <h1 className='text-slate-800 mt-10 text-2xl text-center'>What We Do</h1>
+                      <ol className='text-slate-800'>
+                          <li className='text-slate-800 mt-2 text-center'>Curated Deals: We meticulously curate the best deals, discounts, and offers from a wide range of categories including electronics, fashion, home goods, and more.</li>
+                          <li className='text-slate-800 mt-2 text-center '>Real-Time Updates: Our platform is updated in real-time to ensure you have access to the freshest deals as soon as they go live.</li>
+                          <li className='text-slate-800 mt-2 text-center'>Comprehensive Coverage: Whether you're shopping on Amazon, Flipkart, Myntra, or any other major online store, we've got you covered.</li>
+                      </ol>
+                  </div>
+                  <div className='absolute top-[200px] left-10 h-1/2 w-[1000px] bg-yellow-400 rounded-lg'>
+
+                  </div>
+              </div>
+          </Box>
+
+          <Box sx={{ display: { xs: 'block', sm: 'block', md: 'block', lg: 'none' } }}>
+              <div className='w-screen  flex items-center justify-center'>
+                  <div className='min-h-[300px] w-2/3 mt-10 bg-gray-100 rounded-lg z-10'>
+                      <h1 className='text-slate-800 text-3xl mt-10 text-center'>Welcome to <span className='text-purple-700'>SaveHub!</span> </h1>
+                      <p className='text-slate-800 mt-2 text-center'>At SaveHub, we're dedicated to bringing you the best loot deals from top online shopping destinations like Amazon, Flipkart, Myntra, and more. Our mission is simple: to help you save money while shopping online by providing the most amazing deals and discounts available across the internet.</p>
+                      <h1 className='text-slate-800 mt-10 text-2xl text-center'>Who We Are?</h1>
+                      <p className='text-slate-800 mt-2 text-center'>SaveHub is a passionate team of deal hunters and shopping enthusiasts who understand the thrill of finding a great bargain. We constantly scour the web to bring you the latest and greatest deals, ensuring that you never miss out on an opportunity to save big. </p>
+                      <h1 className='text-slate-800 mt-10 text-2xl text-center'>What We Do</h1>
+                      <ol className='text-slate-800'>
+                          <li className='text-slate-800 mt-2 text-center'>Curated Deals: We meticulously curate the best deals, discounts, and offers from a wide range of categories including electronics, fashion, home goods, and more.</li>
+                          <li className='text-slate-800 mt-2 text-center '>Real-Time Updates: Our platform is updated in real-time to ensure you have access to the freshest deals as soon as they go live.</li>
+                          <li className='text-slate-800 mt-2 text-center'>Comprehensive Coverage: Whether you're shopping on Amazon, Flipkart, Myntra, or any other major online store, we've got you covered.</li>
+                      </ol>
+                  </div>
+                 
+              </div>
+              <div className='w-screen  flex items-center justify-center'>
+
+              <div className='w-2/3 min-h-48  bg-yellow-400 rounded-lg p-10'>
+              <h1 className='text-gray-100 mt-10 text-2xl text-center '>Why Choose SaveHub?</h1>
+                      <ol className='text-gray-100'>
+                          <li className='text-gray-100 mt-2 text-center'>Unmatched Savings: We bring you deals that you won&apos;t find anywhere else, ensuring you get the best value for your money.</li>
+                          <li className='text-gray-100 mt-2 text-center '>User-Friendly Interface: Our website is designed with you in mind, making it easy to navigate and find the deals you&apos;re looking for.</li>
+                          <li className='text-gray-100 mt-2 text-center'>Trusted and Reliable: We pride ourselves on our accuracy and reliability, ensuring that all the deals listed on our site are verified and legitimate.</li>
+                      </ol>
+                      <h1 className='text-gray-100 mt-10 text-2xl text-center'>Join the SaveHub Community</h1>
+                      <p className='text-gray-100 mt-2 text-center'>Become part of the SaveHub community today and start saving on your favorite products. Sign up for our newsletter to receive daily updates on the hottest deals directly in your inbox. Follow us on social media to stay connected and never miss a bargain. </p>
+
+                      <p className='text-gray-100 mt-11'>
+                          Thank you for choosing SaveHub, where saving is just a click away!
+                      </p>
+                      <p className='text-teal-500 mt-10'>
+                          Happy Shopping!
+                      </p>
+                      <p className='text-purple-700 mt-2'>
+                          The SaveHub Team
+                      </p >
+              </div>
+              </div>
+          </Box>
+
+
+          <Box sx={{ display: { xs: 'none', sm: 'none', md: 'none', lg: 'block', mb: '50px' } }}>
+              <div className='relative h-[1000px] w-screen container'>
+                  <div className='absolute left-32   h-[600px] w-[1000px] p-10 bg-gray-100 rounded-lg z-10'>
+
+
+                      <h1 className='text-slate-800 mt-10 text-2xl text-center'>Why Choose SaveHub?</h1>
+                      <ol className='text-slate-800'>
+                          <li className='text-slate-800 mt-2 text-center'>Unmatched Savings: We bring you deals that you won&apos;t find anywhere else, ensuring you get the best value for your money.</li>
+                          <li className='text-slate-800 mt-2 text-center '>User-Friendly Interface: Our website is designed with you in mind, making it easy to navigate and find the deals you&apos;re looking for.</li>
+                          <li className='text-slate-800 mt-2 text-center'>Trusted and Reliable: We pride ourselves on our accuracy and reliability, ensuring that all the deals listed on our site are verified and legitimate.</li>
+                      </ol>
+                      <h1 className='text-slate-800 mt-10 text-2xl text-center'>Join the SaveHub Community</h1>
+                      <p className='text-slate-800 mt-2 text-center'>Become part of the SaveHub community today and start saving on your favorite products. Sign up for our newsletter to receive daily updates on the hottest deals directly in your inbox. Follow us on social media to stay connected and never miss a bargain. </p>
+
+                      <p className='text-slate-800 mt-11'>
+                          Thank you for choosing SaveHub, where saving is just a click away!
+                      </p>
+                      <p className='text-orange-600'>
+                          Happy Shopping!
+                      </p>
+                      <p className='text-purple-700 mt-2'>
+                          The SaveHub Team
+                      </p >
+                  </div>
+                  <div className='absolute top-[200px] right-10 h-1/2 w-[1000px] bg-yellow-400 rounded-lg'>
+
+                  </div>
+              </div>
+          </Box>
+
+        
+        
+
+
+        
+        </div>
+  )
+}
+
+export default AboutUs
