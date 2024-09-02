aaf754489a56c2414374af958eb3964ec8babf23
rEjIn2 <rejin374@gmail.com>
2024-09-02 09:31:21 +0400

    edited Mens

--- src/hooks/useMenProduct.ts
+++ src/hooks/useMenProduct.ts
@@ -1,20 +1,11 @@
 import { useQuery } from 'react-query';
 import axios from 'axios';
 
-const useMenProducts = (category) => {
-  // return useQuery('menProducts', async () => {
-  //   const { data } = await axios.get('/api/getMenClothing',{
-  //     headers: {
-  //       'Content-Type':"application/json",
-  //       'x-api-key': process.env.NEXT_PUBLIC_API_KEY,
-  //     }
-  //   });
-  //   return data;
-  // });
+const useMenProducts = (category: string) => {
   return useQuery(['products', category], async () => {
     const { data } = await axios.get(`/api/getMenClothing?category=${category}`, {
       headers: {
-        'Content-Type': "application/json",
+        'Content-Type': 'application/json',
         'x-api-key': process.env.NEXT_PUBLIC_API_KEY,
       }
     });
