db89fb11089cecda75594d7fa450c6191c556ff5
Rejin <rejin374@gmail.com>
2024-10-13 13:03:22 +0400

    checking country

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,11 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
+import axios from 'axios';
 
 // List of allowed origins (you can expand this)
 const allowedOrigins = ['https://www.savehubonline.com', 'https://savehubonline.com' ,"http://localhost:3001","http://localhost:3000"]
 
-export function middleware(request: NextRequest) {
+export async function middleware(request: NextRequest) {
   if (request.nextUrl.pathname.startsWith('/api/')) {
     // Check for API key
     const apiKey = request.headers.get('x-api-key')
@@ -31,6 +32,16 @@ export function middleware(request: NextRequest) {
     if (clientIp === '::1' || clientIp === '127.0.0.1' || clientIp.startsWith('192.168.') || clientIp.startsWith('10.')) {
       console.log('Local development detected, skipping country check');
       return NextResponse.next();
+    }
+
+    try {
+      const response = await axios.get(`https://ipinfo.io/${clientIp}?token=${process.env.IPINFO_TOKEN}`);
+      const country = response.data.country;
+      console.log(country,"country");
+      
+    } catch (error) {
+      console.log(error);
+      
     }
     // Rate limiting (simple implementation)
     // const ip = request.ip ?? '127.0.0.1'
